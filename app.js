e521b2c2285c61b001e88b0ef491365ce98b7753
Dhara <dhara1.patel@bacancy.com>
2021-02-01 10:56:00 +0530

    commited!

---
+++ app.js
@@ -0,0 +1,39 @@
+const app =Vue.createApp({
+    data() {
+        return {
+            boxASelected : false,
+            boxBSelected : false,
+            boxCSelected : false,
+        };
+    },
+    computed: {
+        boxAClasses() {
+            return {
+                activea : this.boxASelected
+            };
+        },
+        boxBClasses() {
+            return {
+                activeb : this.boxBSelected
+            };
+        },
+        boxCClasses() {
+            return {
+                activec : this.boxCSelected
+            };
+        }
+    },
+    methods : {
+        boxSelected(box) {
+            if(box === 'A') {
+                this.boxASelected = !this.boxASelected;
+            } else if(box === 'B') {
+                this.boxBSelected = !this.boxBSelected;
+            } else if(box === 'C') {
+                this.boxCSelected = !this.boxCSelected;
+            }
+        }
+    }
+});
+
+app.mount('#styling');
